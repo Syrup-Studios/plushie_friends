38bb605451f6b5ba5ecfc20fb200ecc7096417f3
Salad <salad@bysalad.studio>
2026-06-28 20:00:53 +0200

    1.21.1 test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,6 +25,7 @@ stonecutter {
             .forEach { loader -> version("$version-$loader", version).buildscript = "build.$loader.gradle.kts" }
 
         match("1.20.1", "fabric")
+        match("1.21.1", "fabric")
 
         vcsVersion = "1.20.1-fabric"
     }
